d62f162ee2cb2d82efdae22222addf494a6f12b7
kyoungjin2310 <kyoungjin2310@gmail.com>
2023-12-27 22:53:31 +0900

    fix: [modal-팝업] 팝업 한페이지에 같이 나오게 하기

--- src/app/(beforeLogin)/@modal/page.tsx
+++ src/app/(beforeLogin)/@modal/page.tsx
@@ -1,3 +1,62 @@
+"use client";
+
+import { useState } from "react";
+import style from "@/app/(beforeLogin)/@modal/login.module.css";
 export default function Page() {
-  return "패러랠모달";
+  const [id, setId] = useState();
+  const [password, setPassword] = useState();
+  const [message, setmessage] = useState();
+
+  const onSubmit = () => {};
+  const onClickClose = () => {};
+  const onChangeId = () => {};
+  const onChangePassword = () => {};
+  return (
+    <div className={style.modalBackground}>
+      <div className={style.modal}>
+        <div className={style.modalHeader}>
+          <button className={style.closeButton} onClick={onClickClose}>
+            닫기
+          </button>
+          <div>로그인하세요.</div>
+        </div>
+        <form onSubmit={onSubmit}>
+          <div className={style.modalBody}>
+            <div className={style.inputDiv}>
+              <label className={style.inputLabel} htmlFor="id">
+                아이디
+              </label>
+              <input
+                id="id"
+                className={style.input}
+                value={id}
+                onChange={onChangeId}
+                type="text"
+                placeholder=""
+              />
+            </div>
+            <div className={style.inputDiv}>
+              <label className={style.inputLabel} htmlFor="password">
+                비밀번호
+              </label>
+              <input
+                id="password"
+                className={style.input}
+                value={password}
+                onChange={onChangePassword}
+                type="text"
+                placeholder=""
+              />
+            </div>
+          </div>
+          <div className={style.message}>{message}</div>
+          <div className={style.modalFooter}>
+            <button className={style.actionButton} disabled={!id && !password}>
+              로그인하기
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
