f2bc12c0046ac64f5110aff0c2913ec7c4155d41
kyoungjin2310 <kyoungjin2310@gmail.com>
2024-01-04 23:30:41 +0900

    feat: 로그인후 layout 추가

--- src/app/(afterLogin)/layout.tsx
+++ src/app/(afterLogin)/layout.tsx
@@ -1,14 +1,41 @@
-import React, { ReactNode } from "react";
+import { ReactNode } from "react";
+import style from "@/app/(afterLogin)/layout.module.css";
+import Link from "next/link";
 
-interface Props {
-  children: ReactNode;
-}
-
-export default async function AfterLayout({ children }: Props) {
+type Props = { children: ReactNode; modal: ReactNode };
+export default async function AfterLoginLayout({ children, modal }: Props) {
   return (
-    <div>
-      Enter
-      {children}
+    <div className={style.container}>
+      <header className={style.leftSectionWrapper}>
+        <section className={style.leftSection}>
+          <div className={style.leftSectionFixed}>
+            <Link className={style.logo} href="/home">
+              <div className={style.logoPill}>
+                <svg
+                  viewBox="0 0 24 24"
+                  aria-hidden="true"
+                  className="r-4qtqp9 r-yyyyoo r-dnmrzs r-bnwqim r-1plcrui r-lrvibr r-k200y r-18jsvk2 r-5sfk15 r-kzbkwu"
+                >
+                  <g>
+                    <path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z"></path>
+                  </g>
+                </svg>
+              </div>
+            </Link>
+          </div>
+        </section>
+      </header>
+      <div className={style.rightSectionWrapper}>
+        <div className={style.rightSectionInner}>
+          <main className={style.main}>{children}</main>
+          <section className={style.rightSection}>
+            <div className={style.followRecommend}>
+              <h3>팔로우 추천</h3>
+            </div>
+          </section>
+        </div>
+      </div>
+      {modal}
     </div>
   );
 }
