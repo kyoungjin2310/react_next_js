c351b733e0592ad093110aa98de0890d87e2d54f
kyoungjin2310 <kyoungjin2310@gmail.com>
2024-01-06 23:51:09 +0900

    feat: [현재 라우터] 현재 라우터 조회 방법

--- src/app/(afterLogin)/_component/NavMenu.tsx
+++ src/app/(afterLogin)/_component/NavMenu.tsx
@@ -1,7 +1,14 @@
 "use client";
 
+import { useSelectedLayoutSegment } from "next/navigation";
+
 const NavMenu = () => {
-  return <div>Enter</div>;
+  //호출된 레이아웃보다 한단계 아래 폴더(현재 라우터 아는방법)
+  const segment = useSelectedLayoutSegment();
+
+  //그 아래 세부폴더까지 알려면(현재 라우터 아는 방법)
+  //const segments = useSelectedLayoutSegments()
+  return <div></div>;
 };
 
 export default NavMenu;
