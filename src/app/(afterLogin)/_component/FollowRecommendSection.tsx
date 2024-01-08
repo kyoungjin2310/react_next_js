fa027ec43c6301e24c4a05fb708fdcd17ed7656c
kyoungjin2310 <kyoungjin2310@gmail.com>
2024-01-08 19:23:11 +0900

    feat: 팔로우추천, 트렌드 추천 추가

---
+++ src/app/(afterLogin)/_component/FollowRecommendSection.tsx
@@ -0,0 +1,7 @@
+import style from "./folllowRecomendSection.module.css";
+
+const FollowRecommendSection = () => {
+  return <div>Enter</div>;
+};
+
+export default FollowRecommendSection;
---
+++ src/app/(afterLogin)/_component/TrendSection.tsx
@@ -0,0 +1,13 @@
+import style from "./trendSection.module.css";
+
+const TrendSection = () => {
+  return (
+    <div className={style.trendBg}>
+      <div className={style.trend}>
+        <h3>나를 위한 트렌드</h3>
+      </div>
+    </div>
+  );
+};
+
+export default TrendSection;
