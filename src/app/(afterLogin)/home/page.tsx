c4168fd1e10bb7ad6345b3e622d3d6a30ee451f4
kyoungjin2310 <kyoungjin2310@gmail.com>
2024-01-14 00:33:14 +0900

    fix: post 수정

--- src/app/(afterLogin)/home/page.tsx
+++ src/app/(afterLogin)/home/page.tsx
@@ -1,6 +1,6 @@
 import Tab from "@/app/(afterLogin)/home/_component/Tab";
 import PostForm from "./_component/PostForm";
-import Post from "../_component/Post";
+import Post from "@/app/(afterLogin)/_component/Post";
 import TabProvider from "./_component/TabProvider";
 
 export default function Home() {
