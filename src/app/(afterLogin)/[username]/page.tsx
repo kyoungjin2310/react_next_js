e9563c8c47aa184984d3c615e10f2b3d152fe85a
kyoungjin2310 <kyoungjin2310@gmail.com>
2024-01-15 00:56:06 +0900

    feat: 프로필 페이지 추가

--- src/app/(afterLogin)/[username]/page.tsx
+++ src/app/(afterLogin)/[username]/page.tsx
@@ -1,3 +1,34 @@
-export default function Home() {
-  return <div>test</div>;
+import style from "./profile.module.css";
+import Post from "@/app/(afterLogin)/_component/Post";
+import BackButton from "@/app/(afterLogin)/_component/BackButton";
+
+export default function Profile() {
+  const user = {
+    id: "test",
+    nickname: "test name",
+    image:
+      "https://images.unsplash.com/photo-1704996440137-44a1eb3c71ee?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  };
+
+  return (
+    <main className={style.main}>
+      <div className={style.header}>
+        <BackButton />
+        <h3 className={style.headerTitle}>{user.nickname}</h3>
+      </div>
+      <div className={style.userZone}>
+        <div className={style.userImage}>
+          <img src={user.image} alt={user.id} />
+        </div>
+        <div className={style.userName}>
+          <div>{user.nickname}</div>
+          <div>@{user.id}</div>
+        </div>
+        <button className={style.followButton}>팔로우</button>
+      </div>
+      <div>
+        <Post />
+      </div>
+    </main>
+  );
 }
