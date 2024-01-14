8af3fba3364b22cf64061a4ef6f66d80bf5ed818
kyoungjin2310 <kyoungjin2310@gmail.com>
2024-01-15 01:00:29 +0900

    feat: 검색 상세 페이지 추가

--- src/app/(afterLogin)/search/page.tsx
+++ src/app/(afterLogin)/search/page.tsx
@@ -1,3 +1,24 @@
-export default function Home() {
-  return <main>검색페이지</main>;
+import style from "./search.module.css";
+import BackButton from "@/app/(afterLogin)/_component/BackButton";
+import SearchForm from "@/app/(afterLogin)/_component/SearchForm";
+import Post from "@/app/(afterLogin)/_component/Post";
+
+export default function Search() {
+  return (
+    <main className={style.main}>
+      <div className={style.searchTop}>
+        <div className={style.searchZone}>
+          <div className={style.buttonZone}>
+            <BackButton />
+          </div>
+          <div className={style.formZone}>
+            <SearchForm />
+          </div>
+        </div>
+      </div>
+      <div className={style.list}>
+        <Post />
+      </div>
+    </main>
+  );
 }
