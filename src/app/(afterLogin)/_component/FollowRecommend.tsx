e1340d6795380f9f094578a2150da253fe82fc5d
kyoungjin2310 <kyoungjin2310@gmail.com>
2024-01-16 13:01:36 +0900

    feat: 쪽지 보내기 추가

--- src/app/(afterLogin)/_component/FollowRecommend.tsx
+++ src/app/(afterLogin)/_component/FollowRecommend.tsx
@@ -8,13 +8,16 @@ export default function FollowRecommend() {
   const user = {
     id: "elonmusk",
     nickname: "Elon Musk",
-    image: "/yRsRRjGO.jpg",
+    image:
+      "https://pbs.twimg.com/profile_images/1683325380441128960/yRsRRjGO_normal.jpg",
   };
 
   return (
     <div className={style.container}>
       <div className={style.userLogoSection}>
-        <div className={style.userLogo}></div>
+        <div className={style.userLogo}>
+          <img src={user.image} alt={user.id} />
+        </div>
       </div>
       <div className={style.userInfo}>
         <div className={style.title}>{user.nickname}</div>
