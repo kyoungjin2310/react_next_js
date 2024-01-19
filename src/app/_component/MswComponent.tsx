fb1a9eadfd4902fe5190c6957a3473cc5639ca83
kyoungjin2310 <kyoungjin2310@gmail.com>
2024-01-19 22:28:52 +0900

    feat:[목업서버 - msw] 개발할 경우 NEXT_PUBLIC_API_MOCKING=enabled로 설정

--- src/app/_component/MswComponent.tsx
+++ src/app/_component/MswComponent.tsx
@@ -4,6 +4,7 @@ import { useEffect } from "react";
 export const MSWComponent = () => {
   useEffect(() => {
     if (typeof window !== "undefined") {
+      //개발을 할거면 .env에 NEXT_PUBLIC_API_MOCKING=enabled로 설정 아닐때는 다른 값
       if (process.env.NEXT_PUBLIC_API_MOCKING === "enabled") {
         require("@/mocks/browser");
       }
