f5601f7d3e81bda427dccd97bd6b646e800e5073
kyoungjin2310 <kyoungjin2310@gmail.com>
2024-01-19 22:37:04 +0900

    feat: [목업서버 - msw] .env.local에서 NEXT_PUBLIC_을 꼭 붙이기

--- src/app/_component/MswComponent.tsx
+++ src/app/_component/MswComponent.tsx
@@ -5,6 +5,7 @@ export const MSWComponent = () => {
   useEffect(() => {
     if (typeof window !== "undefined") {
       //개발을 할거면 .env.local에 NEXT_PUBLIC_API_MOCKING=enabled로 설정 아닐때는 다른 값
+      //브라우저에서 접근이 가능하도록 NEXT_PUBLIC_을 꼭 붙여야함
       if (process.env.NEXT_PUBLIC_API_MOCKING === "enabled") {
         require("@/mocks/browser");
       }
