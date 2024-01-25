f4d5ac7a533386845e27ab45e1ee58b2a7e24543
kyoungjin2310 <kyoungjin2310@gmail.com>
2024-01-25 14:42:04 +0900

    feat:[react querty - staleTime] fresh -> stale 시간 조정

--- src/app/(afterLogin)/home/_component/PostRecommends.tsx
+++ src/app/(afterLogin)/home/_component/PostRecommends.tsx
@@ -4,12 +4,21 @@ import { useQuery } from "@tanstack/react-query";
 import { getPostRecommends } from "../_lib/getPostRecommends";
 import Post from "../../_component/Post";
 import { Post as IPost } from "@/model/Post";
+import { useEffect } from "react";
 
 export default function PostRecommends() {
   //queryKey가 같아야함
-  const { data } = useQuery<IPost[]>({
+  const { data } = useQuery<IPost>({
     queryKey: ["post", "recommends"],
     queryFn: getPostRecommends,
+    //staleTime : fresh -> stale 시간 조정 단위(ms, Infinity - 항상 fresh)
+    staleTime: 60 * 1000,
   });
-  return data?.map((post) => <Post key={post.postId} post={post} />);
+
+  useEffect(() => {
+    console.log(data);
+  });
+
+  const arr: IPost[] = data ? [data] : [];
+  return arr?.map((post) => <Post key={post.postId} post={post} />);
 }
