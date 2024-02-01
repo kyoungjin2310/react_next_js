c07c6f40ea8115bdc092b131ab75f0cfa87e3733
kyoungjin2310 <kyoungjin2310@gmail.com>
2024-02-02 00:18:26 +0900

    feat: 유저정보 가져오기

--- src/app/(afterLogin)/[username]/page.tsx
+++ src/app/(afterLogin)/[username]/page.tsx
@@ -2,34 +2,40 @@ import style from "./profile.module.css";
 import Post from "@/app/(afterLogin)/_component/Post";
 import BackButton from "@/app/(afterLogin)/_component/BackButton";
 import FollowButton from "../_component/FollowButton";
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from "@tanstack/react-query";
+import { getUser } from "@/app/(afterLogin)/[username]/_lib/getUser";
+import { getUserPosts } from "@/app/(afterLogin)/[username]/_lib/getUserPosts";
+import UserPosts from "./_component/UserPosts";
+import UserInfo from "./_component/UserInfo";
 
-export default async function Profile() {
-  const user = {
-    id: "test",
-    nickname: "test name",
-    image:
-      "https://images.unsplash.com/photo-1704996440137-44a1eb3c71ee?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  };
+type Props = {
+  params: { username: string };
+};
+export default async function Profile({ params }: Props) {
+  const { username } = params;
+  const queryClient = new QueryClient();
+  await queryClient.prefetchQuery({
+    queryKey: ["users", username],
+    queryFn: getUser,
+  });
+  await queryClient.prefetchQuery({
+    queryKey: ["posts", "users", username],
+    queryFn: getUserPosts,
+  });
+  const dehydratedState = dehydrate(queryClient);
 
   return (
     <main className={style.main}>
-      <div className={style.header}>
-        <BackButton />
-        <h3 className={style.headerTitle}>{user.nickname}</h3>
-      </div>
-      <div className={style.userZone}>
-        <div className={style.userImage}>
-          <img src={user.image} alt={user.id} />
+      <HydrationBoundary state={dehydratedState}>
+        <UserInfo username={username} />
+        <div>
+          <UserPosts username={username} />
         </div>
-        <div className={style.userName}>
-          <div>{user.nickname}</div>
-          <div>@{user.id}</div>
-        </div>
-        <FollowButton />
-      </div>
-      <div>
-        <Post />
-      </div>
+      </HydrationBoundary>
     </main>
   );
 }
