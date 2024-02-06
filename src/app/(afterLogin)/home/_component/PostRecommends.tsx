628162ca29af4ea5beee4e41fbde30706d86c617
kyoungjin2310 <kyoungjin2310@gmail.com>
2024-02-06 23:04:31 +0900

    feat: [client - loading] react query loading 공통으로 처리

--- src/app/(afterLogin)/home/_component/PostRecommends.tsx
+++ src/app/(afterLogin)/home/_component/PostRecommends.tsx
@@ -1,45 +1,33 @@
 "use client";
 
-import {
-  InfiniteData,
-  useInfiniteQuery,
-  useQuery,
-} from "@tanstack/react-query";
+import { InfiniteData, useSuspenseInfiniteQuery } from "@tanstack/react-query";
 import { getPostRecommends } from "../_lib/getPostRecommends";
 import Post from "../../_component/Post";
 import { Post as IPost } from "@/model/Post";
 import { Fragment, useEffect } from "react";
 import { useInView } from "react-intersection-observer";
-import styles from "../home.module.css";
 
 export default function PostRecommends() {
   //hasNextPage - 다음페이지가 있으면 true, 없으면 false
-  const {
-    data,
-    fetchNextPage,
-    hasNextPage,
-    isFetching,
-    isPending,
-    isLoading,
-    isError,
-  } = useInfiniteQuery<
-    IPost[],
-    Object,
-    InfiniteData<IPost[]>,
-    //queryKey가 같아야함
-    [_1: string, _2: string],
-    number
-  >({
-    queryKey: ["posts", "recommends"],
-    queryFn: getPostRecommends,
-    //staleTime : fresh -> stale 시간 조정 단위(ms, Infinity - 항상 fresh)
-    staleTime: 60 * 1000,
-    getNextPageParam: (lastPage: IPost[]) => {
-      return lastPage[lastPage.length - 1]?.postId;
-    },
-    //초기 데이터 - react query Action reset 사용시 initialData 있으면 initialData, 없으면 데이터를 새로 가져옴
-    initialPageParam: 0,
-  });
+  const { data, fetchNextPage, hasNextPage, isFetching, isError } =
+    useSuspenseInfiniteQuery<
+      IPost[],
+      Object,
+      InfiniteData<IPost[]>,
+      //queryKey가 같아야함
+      [_1: string, _2: string],
+      number
+    >({
+      queryKey: ["posts", "recommends"],
+      queryFn: getPostRecommends,
+      //staleTime : fresh -> stale 시간 조정 단위(ms, Infinity - 항상 fresh)
+      staleTime: 60 * 1000,
+      getNextPageParam: (lastPage: IPost[]) => {
+        return lastPage[lastPage.length - 1]?.postId;
+      },
+      //초기 데이터 - react query Action reset 사용시 initialData 있으면 initialData, 없으면 데이터를 새로 가져옴
+      initialPageParam: 0,
+    });
 
   //inView - 처음에는 false, 태그가 보이면 true로 변경
   const { ref, inView } = useInView({
@@ -59,39 +47,6 @@ export default function PostRecommends() {
     return "error";
   }
 
-  if (isPending) {
-    return (
-      <div style={{ display: "flex", justifyContent: "center" }}>
-        <svg
-          className={styles.loader}
-          height="100%"
-          viewBox="0 0 32 32"
-          width={40}
-        >
-          <circle
-            cx="16"
-            cy="16"
-            fill="none"
-            r="14"
-            strokeWidth="4"
-            style={{ stroke: "rgb(29, 155, 240)", opacity: 0.2 }}
-          ></circle>
-          <circle
-            cx="16"
-            cy="16"
-            fill="none"
-            r="14"
-            strokeWidth="4"
-            style={{
-              stroke: "rgb(29, 155, 240)",
-              strokeDasharray: 80,
-              strokeDashoffset: 60,
-            }}
-          ></circle>
-        </svg>
-      </div>
-    );
-  }
   return (
     <>
       {data?.pages.map((page, i) => (
