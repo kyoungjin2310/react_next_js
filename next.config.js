93bc435a8e6cd46f2a13b3b6bc423dc2a236cd8e
kyoungjin2310 <kyoungjin2310@gmail.com>
2024-02-28 23:56:34 +0900

    feat:[이미지 src setting]프론트서버주소 이미지 src 사용할때

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  //프론트서버주소 이미지 src 사용할때
+  async rewrites() {
+    return [
+      {
+        source: "/upload/:slug",
+        destination: "http://localhost:9090/upload/:slug",
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
