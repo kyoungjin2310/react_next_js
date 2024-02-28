6c26ca0e960c34c6e285ea1d6c080d79a899790f
kyoungjin2310 <kyoungjin2310@gmail.com>
2024-02-29 00:13:01 +0900

    fix: [cookie] 쿠키 못읽어서 수정

--- src/app/(afterLogin)/_lib/getTrends.ts
+++ src/app/(afterLogin)/_lib/getTrends.ts
@@ -3,6 +3,7 @@ export async function getTrends() {
     next: {
       tags: ["trends"],
     },
+    credentials: "include",
     cache: "no-store",
   });
   // The return value is *not* serialized
