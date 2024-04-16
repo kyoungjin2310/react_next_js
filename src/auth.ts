6e6ee4dd285cc54d14bd538b23563cd9f6abe29d
kyoungjin2310 <kyoungjin2310@gmail.com>
2024-04-16 23:40:48 +0900

    feat: [cookie] 서버 쿠키 공유

--- src/auth.ts
+++ src/auth.ts
@@ -1,6 +1,7 @@
 import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
-import { NextResponse } from "next/server";
+import { cookies } from "next/headers";
+import cookie from "cookie";
 
 export const {
   handlers: { GET, POST },
@@ -22,13 +23,19 @@ export const {
             "Content-Type": "application/json",
           },
           //credentials.username, credentials.password 고정
-
           body: JSON.stringify({
             id: credentials.username,
             password: credentials.password,
           }),
         });
 
+        let setCookie = authResponse.headers.get("Set-Cookie");
+        console.log("set-cookie", setCookie);
+        if (setCookie) {
+          const parsed = cookie.parse(setCookie);
+          // 브라우저에 쿠키를 심어주는 것(프론트에 쿠키를 심으면 개인정보 유출문제)
+          cookies().set("connect.sid", parsed["connect.sid"], parsed);
+        }
         if (!authResponse.ok) {
           return null;
         }
